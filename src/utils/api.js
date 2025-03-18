d0666a0acd34e83cad80b7e600d5997df6bc2728
Intranik <100772353+Intranik@users.noreply.github.com>
2025-03-18 13:51:20 +0100

    Update api.js
    changes url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,10 @@
 
 export function getBaseUrl() {
-  // Get the group number from the hostname to determine the base URL for BE
-  const regex = /webshop\-2025\-(g[0-9]{1,2})\-fe/g;
-  const href = window.location.href;
-  const match = regex.exec(href);
-  console.log(match);
-  if (match) {
-    const group = match[1];
-    return `https://webshop-2025-${group}-be.vercel.app/`;
+  if (!href.includes('localhost') {
+    return "https://be-grupp-2.vercel.app/"
   }
   return "http://localhost:3000/";
 }
-
 export async function fetchProducts(endpoint = "api/products") {
   //! DONT USE THIS IN PRODUCTION
   const url = `${getBaseUrl()}${endpoint}`;
