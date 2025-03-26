ea1262bc6ecf4e2faf633d336b6d061a9231b9f1
OmsUgan <Omer.Ugan@yh.nackademin.se>
2025-03-26 23:51:40 +0100

    add login logic, dashboard ui

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -1,8 +1,17 @@
-const adminLogin = async () => {
+const loginCredentials = async (credentials) => {
+        const response = await axios.post("https://webshopbackend.vercel.app/auth/signin", credentials);
+        console.log(response);
+    
+        if (response) {
+            alert("Inloggning lyckades")
+        } 
+    }
+
+document.getElementById("btn-login").addEventListener("click", () => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+    const userCredentials = { email: email, password: password }
 
-}
-
-document.getElementById("btn-login").addEventListener("click", adminLogin);
+    loginCredentials(userCredentials);
+});
