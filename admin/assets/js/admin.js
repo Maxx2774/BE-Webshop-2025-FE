a7985b1ec9404966baa6253c60b0514c9831a0f3
Ömer Ugan <Omer.Ugan@inadra.se>
2025-03-26 19:17:37 +0100

    add admin login page ui

---
+++ admin/assets/js/admin.js
@@ -0,0 +1,8 @@
+const adminLogin = async () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+
+}
+
+document.getElementById("btn-login").addEventListener("click", adminLogin);
