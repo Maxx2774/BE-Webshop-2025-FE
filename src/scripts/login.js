b64f87fcbed2a6f74956672fc3580ef822de6fd7
cfilip80 <christian.filip@hotmail.com>
2025-03-29 14:01:36 +0100

    test

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,23 +1,35 @@
-document.addEventListener("DOMContentLoaded", initLogin);
+// document.addEventListener("DOMContentLoaded", initLogin);
 
-function initLogin() {
-  const loginForm = document.getElementById("loginForm");
+// function initLogin() {
+//   const loginForm = document.getElementById("loginForm");
 
-  loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    handleLogin();
-  });
-}
+//   loginForm.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     handleLogin();
+//   });
+// }
 
-function handleLogin() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+// function handleLogin() {
+//   const username = document.getElementById("username").value;
+//   const password = document.getElementById("password").value;
 
-  // Basic demo login - NOT SECURE
-  if (username === "admin" && password === "admin") {
-    window.location.href = "admin.html";
-  } else {
-    alert("Invalid credentials: LOGGING IN ANYWAY");
-    window.location.href = "admin.html";
+//   // Basic demo login - NOT SECURE
+//   if (username === "admin" && password === "admin") {
+//     window.location.href = "admin.html";
+//   } else {
+//     alert("Invalid credentials: LOGGING IN ANYWAY");
+//     window.location.href = "admin.html";
+//   }
+// }
+import { fetchProducts } from '../utils/api.js'
+
+async function getAndLogProducts() {
+  try {
+    const products = await fetchProducts();
+    console.log(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
   }
 }
+
+getAndLogProducts();
