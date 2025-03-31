ccf5bbb06b6fc5c3a940b51754aee122d375d106
OmsUgan <Omer.Ugan@yh.nackademin.se>
2025-03-31 19:09:43 +0200

    add cards to dashboard home page

--- admin/assets/js/index.js
+++ admin/assets/js/index.js
@@ -1,3 +1,7 @@
-import { verifyToken} from './services.js';
+import { verifyToken, GetAsync, baseUrl } from './services.js';
 
 await verifyToken();
+
+const getProducts = await GetAsync(`${baseUrl}/products`);
+
+document.getElementById("product-count").textContent = getProducts.data.length;
